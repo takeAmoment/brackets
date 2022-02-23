82ae292693543d7946deb721a2b77ece339d6f96
alpekhota <alekspekhota@gmail.com>
2022-02-23 13:26:17 +0100

    feat: create condition, which check if openBrackets contains elements of str

--- src/index.js
+++ src/index.js
@@ -8,4 +8,10 @@ module.exports = function check(str, bracketsConfig) {
     openBrackets.push(bracketsConfig[i][0]);
     brackets[bracketsConfig[i][1]] = bracketsConfig[i][0];
   }
+
+  for(let i= 0; i < str.length; i++){
+    if(openBrackets.includes(str[i])){
+      
+    }
+  }
 }
