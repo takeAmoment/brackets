92ba12374b3366d8914aabba1c7253f0cb1fb8fb
alpekhota <alekspekhota@gmail.com>
2022-02-23 13:21:21 +0100

    feat: add elements to openBrackets and bracket object

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ module.exports = function check(str, bracketsConfig) {
   let brackets = {};
   let stuck = [];
   let flag = false;
+
+  for (let i = 0; i < bracketsConfig.length; i++){
+    openBrackets.push(bracketsConfig[i][0]);
+    brackets[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
 }
